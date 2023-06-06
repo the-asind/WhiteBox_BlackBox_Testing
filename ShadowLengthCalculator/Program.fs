bcf1866e8eb13c44a42eafd1df6a05904996a1be
the-asind <84527186+the-asind@users.noreply.github.com>
2023-06-06 17:37:31 +0700

    Add ShadowLengthCalculator, add WhiteBox for that

---
+++ ShadowLengthCalculator/Program.fs
@@ -0,0 +1,56 @@
+﻿module ShadowLengthCalculator.Program
+open System
+
+type Segment = int * int
+
+let parseSegment (input: string) : Option<Segment> =
+    match input.Split(' ') with
+    | [| start; end' |] ->
+        match Int32.TryParse(start), Int32.TryParse(end') with
+        | (true, parsedStart), (true, parsedEnd) ->
+            Some (parsedStart, parsedEnd)
+        | _ ->
+            None
+    | _ ->
+        None
+
+let readSegments () : Segment list =
+    let rec loop (segments: Segment list) =
+        let input = Console.ReadLine()
+        if String.IsNullOrWhiteSpace(input) then
+            segments
+        else
+            match parseSegment input with
+            | Some segment ->
+                loop (segment :: segments)
+            | None ->
+                printfn "Invalid input format"
+                loop segments
+    loop []
+
+let combineSegments (segments : Segment list) =
+    segments
+    |> List.sortBy fst
+    |> List.fold (fun combined (start, end') ->
+        match combined with
+        | (prevStart, prevEnd) :: rest ->
+            if start <= prevEnd then
+                (prevStart, max end' prevEnd) :: rest
+            else
+                (start, end') :: combined
+        | _ ->
+            [(start, end')]
+    ) []
+
+let calculateTotalLength (segments : Segment list) =
+    segments
+    |> List.map (fun (start, end') -> end' - start)
+    |> List.sum
+
+let main() =
+    printfn "Enter segments (start and end) separated by a space:"
+    let segments = readSegments( )
+    let combinedSegments = combineSegments segments
+    let totalLength = calculateTotalLength combinedSegments
+    printfn $"Combined segments: %A{combinedSegments}"
+    printfn $"Total length: %d{totalLength}"
---
+++ ShadowLengthCalculator/WhiteBoxTests.fs
@@ -0,0 +1,92 @@
+module ShadowLengthCalculator.WhiteBoxTests
+
+open System
+open System.IO
+open NUnit.Framework
+open Program
+
+[<TestFixture>]
+type SegmentTests() =
+    [<Test>]
+    member this.``parseSegment should return Some segment for valid input``() =
+        let input = "1 5"
+        let expectedSegment = Some (1, 5)
+        let actualSegment = parseSegment input
+        Assert.AreEqual(expectedSegment, actualSegment)
+
+    [<Test>]
+    member this.``parseSegment should return None for invalid input``() =
+        let input = "1 invalid"
+        let expectedSegment = None
+        let actualSegment = parseSegment input
+        Assert.AreEqual(expectedSegment, actualSegment)
+
+    [<Test>]
+    member this.``combineSegments should combine overlapping segments``() =
+        let segments = [(1, 5); (4, 6); (7, 10)]
+        let expectedCombinedSegments = [(7, 10); (1, 6)]
+        let actualCombinedSegments = combineSegments segments
+        CollectionAssert.AreEqual(expectedCombinedSegments, actualCombinedSegments)
+    [<Test>]
+    member this.``combineSegments should handle non-overlapping segments``() =
+        let segments = [(1, 5); (6, 8); (9, 12)]
+        let expectedCombinedSegments = [(9, 12); (6, 8); (1, 5)]
+        let actualCombinedSegments = combineSegments segments
+        CollectionAssert.AreEqual(expectedCombinedSegments, actualCombinedSegments)
+
+    [<Test>]
+    member this.``calculateTotalLength should return the sum of segment lengths``() =
+        let segments = [(1, 5); (4, 6); (7, 10)]
+        let expectedTotalLength = 8
+        let combinedSegments = combineSegments segments
+        let actualTotalLength = calculateTotalLength combinedSegments
+        Assert.AreEqual(expectedTotalLength, actualTotalLength)
+        
+    [<Test>]
+    member this.``readSegments should return empty list when no input is provided``() =
+        let mockInput = new StringReader("")
+        Console.SetIn mockInput
+
+        let segments = readSegments()
+
+        Assert.IsEmpty(segments)
+
+    [<Test>]
+    member this.``readSegments should skip invalid input and continue``() =
+        let mockInput = new StringReader("1 5\ninvalid\n4 6\ninvalid\n7 10\n")
+        Console.SetIn mockInput
+
+        let segments = readSegments()
+
+        CollectionAssert.AreEqual([(7, 10); (4, 6); (1, 5)], segments)
+
+    [<Test>]
+    member this.``calculateTotalLength should return 0 for an empty list``() =
+        let segments = []
+
+        let totalLength = calculateTotalLength segments
+
+        Assert.Zero(totalLength)
+
+    [<Test>]
+    member this.``calculateTotalLength should return correct total length for multiple segments``() =
+        let segments = [(1, 5); (4, 6); (7, 10)]
+
+        let combinedSegments = combineSegments segments
+        let actualTotalLength = calculateTotalLength combinedSegments
+        Assert.AreEqual(8, actualTotalLength)
+
+    [<Test>]
+    member this.``main should print the correct output``() =
+        let mockInput = new StringReader("1 5\n4 6\n7 10\n")
+        Console.SetIn mockInput
+
+        let writer = new StringWriter()
+        Console.SetOut writer
+
+        main()
+
+        let expectedOutput = "Enter segments (start and end) separated by a space:\r\nCombined segments: [(7, 10); (1, 6)]\r\nTotal length: 8"
+        let output = writer.ToString().Trim()
+
+        Assert.AreEqual(expectedOutput, output)
